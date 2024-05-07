507f414eb87c99fa849a572f795c459c98c4e567
paugomezm <pgm5060@gmail.com>
2024-05-07 23:51:33 +0200

    .

--- paugm-portfolio/src/Sections.js
+++ paugm-portfolio/src/Sections.js
@@ -1,11 +1,12 @@
 import Experience from "./Blocks/Experience";
 import Studies from "./Blocks/Studies";
+import Profile from "./Sidebar/Profile";
 
 export default function Sections() {
     return (
         <div className="sections">
             <div className="first-section">
-            
+            <Profile/>
             </div>
             <div className="second-section">
             <Experience/>
---
+++ paugm-portfolio/src/Sidebar/Profile.js
@@ -0,0 +1,13 @@
+import profileimage from './profile1.jpg';
+
+
+export default function Profile() {
+    return (
+        <div>
+            <div className='profile-section1'>
+                <img className='profile-img' src={profileimage} alt='#' />
+                <h4>Pau</h4>
+            </div>
+        </div>
+    )
+}
