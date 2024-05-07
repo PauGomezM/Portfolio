47837505975d4206dbc944ea06d25573feefb6ec
paugomezm <pgm5060@gmail.com>
2024-05-07 21:58:11 +0200

    .

--- paugm-portfolio/src/Sections.js
+++ paugm-portfolio/src/Sections.js
@@ -5,27 +5,7 @@ export default function Sections() {
     return (
         <div className="sections">
             <div className="first-section">
-            <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
-                <p>sdafdgdfg</p>
+            
             </div>
             <div className="second-section">
             <Experience/>
