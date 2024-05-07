af2037b26f8b9223dc0f7be588fe358ee38de271
paugomezm <pgm5060@gmail.com>
2024-05-07 17:26:09 +0200

    .

--- paugm-portfolio/src/Blocks/Experience.js
+++ paugm-portfolio/src/Blocks/Experience.js
@@ -4,7 +4,10 @@ export default function Experience() {
     return (
         <div className="experience">
             <div>
-                <h2>Digital Marketing Specialist at Rentals United</h2>
+                <div className="experience-title-company">
+                    <h2>Digital Marketing Specialist</h2>
+                    <h5>Rentals United</h5>
+                </div>
                 <p></p>
                 <ul>
                     <li>Created multiple landing pages using HubSpot and programming knowledge of HTML, CSS and JavaScript, as well as improving
@@ -17,20 +20,23 @@ export default function Experience() {
                 </ul>
             </div>
             <div>
-                <h2>Digital Marketing Specialist at Artfishing</h2>
+                <div className="experience-title-company">
+                    <h2>Digital Marketing Specialist</h2>
+                    <h5>Artfishing.SL</h5>
+                </div>
                 <p></p>
                 <ul>
-                    <li>Automated and created more than 100 email marketing campaigns to introduce new products of the Yuki and 
-                    Fishus brands for clients from all over Europe resulting in a 16% boost in brand awareness.</li>
+                    <li>Automated and created more than 100 email marketing campaigns to introduce new products of the Yuki and
+                        Fishus brands for clients from all over Europe resulting in a 16% boost in brand awareness.</li>
                     <li>Managed relationships with international sales representatives, retailers and customers.</li>
-                    <li>Developed Social Media strategies managing Yuki & Fishus brands, creating engaging content 
-                    automating and scheduling posts, resulting in a 250% increase over the past 3 years.</li>
-                    <li>Managed a 50000€ budget for sponsorships, World Champions, and influencers, in coordination 
-                    with the accounting department, invoicing department, and export department.</li>
-                    <li>Provided graphic content having provided +1000 professional photos, improving Photoshop, 
-                    Lightroom knowledge, and photography skills to develop the Yuki and Fishus Catalogues every year.</li>
-                    <li>Wrote +500 product descriptions on the yukicompetition website, applying SEO, improving copyrighting 
-                    skills, programming with HTML & CSS in most cases, and using  Google Analytics to analyze our audience.</li>
+                    <li>Developed Social Media strategies managing Yuki & Fishus brands, creating engaging content
+                        automating and scheduling posts, resulting in a 250% increase over the past 3 years.</li>
+                    <li>Managed a 50000€ budget for sponsorships, World Champions, and influencers, in coordination
+                        with the accounting department, invoicing department, and export department.</li>
+                    <li>Provided graphic content having provided +1000 professional photos, improving Photoshop,
+                        Lightroom knowledge, and photography skills to develop the Yuki and Fishus Catalogues every year.</li>
+                    <li>Wrote +500 product descriptions on the yukicompetition website, applying SEO, improving copyrighting
+                        skills, programming with HTML & CSS in most cases, and using  Google Analytics to analyze our audience.</li>
                     <li>Worked with B2B Software categorizing and organizing products in our e-commerce, as well as solving technical issues.</li>
                 </ul>
             </div>
