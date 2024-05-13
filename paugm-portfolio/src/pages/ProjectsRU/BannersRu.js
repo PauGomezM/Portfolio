b367d7e56cb4f741fbb403cdc7525427e5444b6e
paugomezm <pgm5060@gmail.com>
2024-05-13 20:29:09 +0200

    .

--- paugm-portfolio/src/pages/ProjectsRU/BannersRu.js
+++ paugm-portfolio/src/pages/ProjectsRU/BannersRu.js
@@ -1,3 +1,11 @@
+import BannerOne from '../ru-images/banner1.png';
+import BannerTwo from '../ru-images/banner2.png';
+import BannerThree from '../ru-images/banner3.png';
+import BannerFour from '../ru-images/banner4.png';
+import BannerFive from '../ru-images/banner5.png';
+import BannerSix from '../ru-images/banner6.png';
+import BannerSeven from '../ru-images/banner7.png';
+import BannerEight from '../ru-images/str-banner.png';
 
 export default function Banners() {
     return (
@@ -5,7 +13,14 @@ export default function Banners() {
             <div className="projects-section">
                 <h4>Banners</h4>
                 <div className="projects-banners">
-                    <img className="banner"/>
+                    <img src={BannerOne} alt='banner' className="banner"/>
+                    <img src={BannerTwo} alt='banner' className="banner"/>
+                    <img src={BannerThree} alt='banner' className="banner"/>
+                    <img src={BannerFour} alt='banner' className="banner"/>
+                    <img src={BannerFive} alt='banner' className="banner"/>
+                    <img src={BannerSix} alt='banner' className="banner"/>
+                    <img src={BannerSeven} alt='banner' className="banner"/>
+                    <img src={BannerEight} alt='banner' className="banner"/>
                 </div>
             </div>
         </div>
