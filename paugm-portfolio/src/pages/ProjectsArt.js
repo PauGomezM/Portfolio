9d45506db07097ea09375b616468e22854dcea1a
paugomezm <pgm5060@gmail.com>
2024-05-18 18:22:34 +0200

    .

--- paugm-portfolio/src/pages/ProjectsArt.js
+++ paugm-portfolio/src/pages/ProjectsArt.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PhotographeisArt from "./ProjectsArt/PhotographiesArt";
+import PhotographiesArt from "./ProjectsArt/PhotographiesArt";
 
 export default function ProjectsArt() {
     return (
         <div className="projects-art">
             <Link to='/'><button>&#129028; Go back</button></Link>
-            <PhotographeisArt/>
+            <PhotographiesArt/>
             <Link to='/'><button>&#129028; Go back</button></Link>
         </div>
     )
--- paugm-portfolio/src/pages/ProjectsArt/PhotographiesArt.js
+++ paugm-portfolio/src/pages/ProjectsArt/PhotographiesArt.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function PhotographeisArt() {
+export default function PhotographiesArt() {
     return (
         <div>
-            
+            <div className="projects-section">
+                <h4>Wordpress Landing Pages</h4>
+            </div>
         </div>
     )
 }
